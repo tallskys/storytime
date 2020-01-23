114dbdffe4bac530cdafd6825267da182af52542
tallskys <tallsky89@gmail.com>
2020-01-23 14:53:50 -0500

    the start

---
+++ src/layouts/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { navigate, StaticQuery, graphql } from 'gatsby';
+import Swipeable from 'react-swipeable';
+import Transition from '../components/transition';
+
+import './index.css';
+
+
+class TemplateWrapper extends Component {
+  NEXT = [13, 32, 39];
+  PREV = 37;
+
+  swipeLeft = () => {
+    this.navigate({ keyCode: this.NEXT[0] });
+  };
+
+  swipeRight = () => {
+    this.navigate({ keyCode: this.PREV });
+  };
+
+  navigate = ({ keyCode }) => {
+    const now = this.props.data.slide.index;
+    const slidesLength = this.props.slidesLength;
+
+    if (now) {
+      if (keyCode === this.PREV && now === 1) {
+        return false;
+      } else if (this.NEXT.indexOf(keyCode) !== -1 && now === slidesLength) {
+        return false;
+      } else if (this.NEXT.indexOf(keyCode) !== -1) {
+        navigate(`/${now + 1}`);
+      } else if (keyCode === this.PREV) {
+        navigate(`/${now - 1}`);
+      }
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.navigate);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.navigate);
+  }
+
+  render() {
+    const { location, children, site } = this.props;
+
+    return (
+      <div>
+        <Swipeable
+          onSwipedLeft={this.swipeLeft}
+          onSwipedRight={this.swipeRight}
+        >
+          <Transition location={location}>
+            <div id="slide" style={{'width': '100%'}}>{children}</div>
+          </Transition>
+        </Swipeable>
+      </div>
+    );
+  }
+}
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.node,
+  data: PropTypes.object,
+};
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query IndexQuery {
+        allSlide {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <TemplateWrapper
+        site={data.site}
+        slidesLength={data.allSlide.edges.length}
+        {...props}
+      />
+    )}
+  />
+);
---
+++ src/slides/01-intro.md
@@ -0,0 +1,25 @@
+# The start
+
+this is where it began
+
+---
+
+> Inscrutable icons litter the face of the devices even though the research
+> community has long demonstrated that people cannot remember the meaning of
+> more than a small number of icons […] Who can remember what each icon
+> means? Not me.
+> <cite>Don Norman</cite>
+
+---
+
+# 🤫
+
+---
+
+## Slides are written in Markdown!
+
+Here's the source of the first slide:
+
+    # Gatsby Deck
+
+    Create presentations using Gatsby & React.
